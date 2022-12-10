94ceac0301287aec168f7d8859becba7901ff6e0
Purcari <sabina.motrescu@gmail.com>
2022-12-10 11:31:54 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.text.SimpleDateFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        String CNP = "6211220080026";
+        if (CNP.length() != 13) ;
+        {
+            System.out.println("This is not a valid CNP");
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
