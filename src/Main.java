c4282f4a03d62df9ed10795827bf524b92eacc1a
Purcari <sabina.motrescu@gmail.com>
2022-12-17 10:04:50 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,71 @@
 import java.text.SimpleDateFormat;
 
 public class Main {
-    public static void main(String[] args) {
-        String CNP = "6211220080026";
-        if (CNP.length() != 13) ;
-        {
-            System.out.println("This is not a valid CNP");
+    public static boolean isCNPValid(int length) {
+        boolean flag = true;
+        if (length != 13) {
+            flag = false;
         }
+        return flag;
+    }
+    public static String getYearPrefix(char firstNumber) {
+        String prefix = "";
+        switch (firstNumber){
+            case '1':
+                prefix = "19";
+                break;
+            case '2':
+                prefix = "19";
+                break;
+            case '3':
+                prefix = "18";
+                break;
+            case '4':
+                prefix = "18";
+                break;
+            case '5':
+                prefix = "20";
+                break;
+            case '6':
+                prefix = "20";
+                break;
+            default:
+                prefix ="INVALID";
+                break;
+        }
+        return prefix;
+    }
+
+    public static char returnSexOfPerson(char firstNumber) {
+        char sex;
+        if (firstNumber == '1' ||
+                firstNumber == '3' ||
+                firstNumber == '5' ||
+                firstNumber == '7') {
+            sex = 'M';
+        } else {
+            sex = 'F';
+        }
+        return sex;
+    }
+
+    public static String getBirthDateFromCnpAsString(String CNP) {
+        String dateOfBirth = CNP.substring(1,7);
+        String prefix = getYearPrefix(CNP.charAt(0));
+
+        dateOfBirth = prefix + dateOfBirth;
+        return dateOfBirth.substring(6,8) + "-" + dateOfBirth.substring(4,6) + "-" + dateOfBirth.substring(0,4);
+    }
+
+
+
+    public static void main(String[] args) {
+        String CNP = "2931111050098";
+        System.out.println(getBirthDateFromCnpAsString(CNP));
+
+
+
 
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
